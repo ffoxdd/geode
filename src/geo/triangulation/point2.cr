61dbfadd62f524a16c9fdc9b74c5e4db8b7a2ce1
Dan Fox <dhf2001@gmail.com>
2018-12-04 17:46:38 -0500

    Pull out Vector3 to its own class

--- src/geo/triangulation/point2.cr
+++ src/geo/triangulation/point2.cr
@@ -14,20 +14,4 @@ struct Geo::Triangulation::Point2
   def Point2.join(p1, p2)
     Vector3.cross(p1, p2)
   end
-
-  private struct Vector3
-    def initialize(@coordinates = {0.0, 0.0, 1.0})
-    end
-
-    getter coordinates
-    delegate :[], to: coordinates
-
-    def Vector3.cross(v1, v2)
-      Vector3.new({
-         v1[1] * v2[2] - v1[2] * v2[1],
-        -v1[0] * v2[2] + v1[2] * v2[0],
-         v1[0] * v2[1] - v1[1] * v2[0]
-      })
-    end
-  end
 end
--- src/geo/triangulation/vector3.cr
+++ src/geo/triangulation/vector3.cr
@@ -1,1 +1,15 @@
+struct Geo::Triangulation::Vector3
+  def initialize(@coordinates = {0.0, 0.0, 1.0})
+  end
 
+  getter coordinates
+  delegate :[], to: coordinates
+
+  def Vector3.cross(v1, v2)
+    Vector3.new({
+       v1[1] * v2[2] - v1[2] * v2[1],
+      -v1[0] * v2[2] + v1[2] * v2[0],
+       v1[0] * v2[1] - v1[1] * v2[0]
+    })
+  end
+end
