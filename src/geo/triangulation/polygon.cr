d7c51abeec367c0f21bccabe0a0abd64b06c73ff
Dan Fox <dhf2001@gmail.com>
2018-12-09 18:17:50 -0500

    Use iterator chaining to get adjacent vertices

--- src/geo/triangulation/polygon.cr
+++ src/geo/triangulation/polygon.cr
@@ -7,11 +7,15 @@ class Geo::Triangulation::Polygon
   end
 
   private def each_edge
-    EdgeIterator.new(@vertices)
+    @vertices.each
+      .cycle
+      .cons(2, true)
+      .first(@vertices.size)
+      .map { |vertices| Edge.new(vertices) }
   end
 
   private class Edge
-    def initialize(@vertices : Tuple(Point2, Point2))
+    def initialize(@vertices : Array(Point2))
     end
 
     def can_see?(point)
@@ -19,36 +23,11 @@ class Geo::Triangulation::Polygon
     end
 
     private def edge_line
-      Point2.join(*@vertices)
+      Point2.join(@vertices[0], @vertices[1])
     end
 
     private def incident_line(point)
       Point2.join(@vertices[0], point)
-    end
-  end
-
-  private class EdgeIterator
-    include Iterator(Edge)
-
-    def initialize(@vertices : Array(Point2))
-      @index = 0
-    end
-
-    def next
-      return stop if @index > (@vertices.size - 1)
-      current_edge.tap { @index += 1 }
-    end
-
-    def rewind
-      @index = 0
-    end
-
-    private def current_edge
-      Edge.new({@vertices[@index], @vertices[next_index]})
-    end
-
-    private def next_index
-      (@index + 1) % @vertices.size
     end
   end
 end
